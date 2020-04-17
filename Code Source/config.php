df9cc81e378d098774df26cfa23cb746943c0b0b
Ayoub Mançour Billah <61604554+AyoubMB06@users.noreply.github.com>
2020-04-17 02:00:18 +0100

    Update config.php

--- Code Source/config.php
+++ Code Source/config.php
@@ -1,9 +1,9 @@
 <?php
 
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'ecom');
+    define('DB_SERVER', '"localhost";');
+    define('DB_USERNAME', 'id12786864_root');
+    define('DB_PASSWORD', '|$#OG58F>@SruI8F');
+    define('DB_NAME', 'id12786864_ecom');
      
 	try{
 		$BD = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
