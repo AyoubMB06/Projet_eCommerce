e62ca07076320f70d4a5c70266f2819c81bb0d6c
Ayoub Mançour Billah <61604554+AyoubMB06@users.noreply.github.com>
2020-04-17 06:51:20 +0100

    Update config.php

--- Code Source/config.php
+++ Code Source/config.php
@@ -1,8 +1,8 @@
 <?php
 
-    define('DB_SERVER', '"localhost";');
+    define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'id12786864_root');
-    define('DB_PASSWORD', '|$#OG58F>@SruI8F');
+    define('DB_PASSWORD', 'HRi\MoIZ0F_=TRx0');
     define('DB_NAME', 'id12786864_ecom');
      
 	try{
